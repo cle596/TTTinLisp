ba0cf4ae5354d77ce1aaad7758bee3cc71ded1cd
changhan <changhanlee@gmail.com>
2016-06-25 19:26:47 -0400

    progress so painful

--- fun.rkt
+++ fun.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(require (planet williams/describe/describe))
+
 (displayln "Time To Tic Tac Toe\n")
 
 (struct node (board turn))
@@ -12,20 +14,19 @@
 (define (add_space str)
   (string-join (map char_to_str (string->list str))))
 
-(define (create_row node x)
-  (add_newline (substring (add_space (node-board node)) x (+ x 5))))
+(define (create_row str x)
+  (add_newline (substring str x (+ x 5))))
 
 (define (nprint node) 
   (displayln
    (string-join
-    (map create_row (list node node node) (list 0 6 12)) "")))
+    (map create_row
+         (build-list 3 (lambda (x) (add_space (node-board node))))
+         (list 0 6 12)) "")))
 
 (define (sanitize i) (
                       if (and (> (string->number i) 0) (< (string->number i) 10))
                          #t #f))
-
-(define root (node (make-string 9 #\.) "x"))
-(nprint root)
 
 (define (update node move)
   (string-join
@@ -34,10 +35,29 @@
     (node-turn node)
     (substring (node-board node) move 9)) ""))
 
-(define child (struct-copy node root [board (update root 5)] [turn "o"]))
+(define root (node (make-string 9 #\.) "x"))
+(nprint root)
 
-(nprint child)
+#|
+(nprint (struct-copy node root
+                     [board (update root (string->number (read-string 1)))]
+                     [turn "o"]))
 
-;(sanitize (read-string 1))
 
 
+
+|#
+(for ([i (build-list 10 values)])
+  (list (display "Your move(1-9): ")
+        (let ([myport (open-input-string (read-string 2))])  
+        (nprint (struct-copy node root
+                             [board (update root (read myport))]
+                             [turn "o"]))
+        )))
+
+
+
+
+;(read-string 1)
+
+;(read-char 1)
